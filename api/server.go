512c8c15a49f45c958612c31f4f42093c793e5f8
DMV-Nicolas <dmvnicolas@gmail.com>
2023-12-04 15:26:01 -0500

    Adds environment variables with viper and starts the server

--- api/server.go
+++ api/server.go
@@ -1,10 +1,25 @@
 package api
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+
+	db "github.com/DMV-Nicolas/DevoraTasks/db/sqlc"
+	"github.com/gorilla/mux"
+)
 
 type Server struct {
+	db     *db.Queries
 	router *mux.Router
 }
 
-func NewServer() {
+func NewServer(db *db.Queries) *Server {
+	server := &Server{db: db}
+	router := mux.NewRouter()
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return http.ListenAndServe(address, server.router)
 }
--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"log"
 
+	"github.com/DMV-Nicolas/DevoraTasks/api"
+	db "github.com/DMV-Nicolas/DevoraTasks/db/sqlc"
+	"github.com/DMV-Nicolas/DevoraTasks/util"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// load config for environment variables
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+
+	// connect to the database
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to database:", err)
+	}
+
+	db := db.New(conn)
+	server := api.NewServer(db)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("Cannot start server: ", err)
+	}
 }
